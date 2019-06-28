bdef759fd36f2b1b2240872a85eef0a996b30c0b
Reinisch <kvv.bsuir@gmail.com>
2019-06-28 21:17:58 +0300

    Added kill creature spell for testing, added some spell conditions

---
+++ Assets/Scripts/Core/Conditions/Unit/TargetUnitIsCreature.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Core.Conditions
+{
+    [UsedImplicitly, CreateAssetMenu(fileName = "Target Unit Is Creature", menuName = "Game Data/Conditions/Unit/Target Is Creature", order = 3)]
+    public sealed class TargetUnitIsCreature : Condition
+    {
+        protected internal override bool IsApplicable => TargetUnit != null && base.IsApplicable;
+
+        protected internal override bool IsValid
+        {
+            get
+            {
+                bool isValid = TargetUnit is Creature;
+                return base.IsValid && isValid;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Conditions/Unit/TargetUnitIsPlayer.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Core.Conditions
+{
+    [UsedImplicitly, CreateAssetMenu(fileName = "Target Unit Is Player", menuName = "Game Data/Conditions/Unit/Target Is Player", order = 4)]
+    public sealed class TargetUnitIsPlayer : Condition
+    {
+        protected internal override bool IsApplicable => TargetUnit != null && base.IsApplicable;
+
+        protected internal override bool IsValid
+        {
+            get
+            {
+                bool isValid = TargetUnit is Player;
+                return base.IsValid && isValid;
+            }
+        }
+    }
+}
