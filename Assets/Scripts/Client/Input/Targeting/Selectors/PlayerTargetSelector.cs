9e2a833027e2a95fc0519d25b0a9e2f701633456
Reinisch <kvv.bsuir@gmail.com>
2019-06-30 12:50:40 +0300

    Default tab target selects only alive units infront by 50 degrees

--- Assets/Scripts/Client/Input/Targeting/Selectors/PlayerTargetSelector.cs
+++ Assets/Scripts/Client/Input/Targeting/Selectors/PlayerTargetSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Core;
+using UnityEngine;
 
 namespace Client
 {
@@ -14,6 +15,7 @@ namespace Client
             private readonly Player referer;
 
             private int bestTargetPrevousIndex;
+            private float bestTargetDistance;
 
             public Unit BestTarget { get; private set; }
 
@@ -23,6 +25,21 @@ namespace Client
                 this.previousTargets = previousTargets;
                 this.options = options;
                 this.referer = referer;
+
+                switch (options.Distance)
+                {
+                    case TargetingDistance.Any:
+                        bestTargetPrevousIndex = int.MaxValue;
+                        break;
+                    case TargetingDistance.Near:
+                        bestTargetDistance = float.MaxValue;
+                        break;
+                    case TargetingDistance.Far:
+                        bestTargetDistance = float.MinValue;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(options.Distance));
+                }
             }
 
             public void Dispose()
@@ -54,11 +71,53 @@ namespace Client
                 if (!unit.InRangeSqr(referer, settings.TargetRangeSqr))
                     return;
 
-                int unitPreviousIndex = previousTargets.IndexOf(unit);
-                if (BestTarget == null || unitPreviousIndex < bestTargetPrevousIndex)
+                if (!options.DeathState.HasTargetFlag(TargetingDeathState.Alive) && unit.IsAlive)
+                    return;
+
+                if (!options.DeathState.HasTargetFlag(TargetingDeathState.Dead) && unit.IsDead)
+                    return;
+
+                if (options.MaxReferringAngle < 180.0f)
                 {
-                    BestTarget = unit;
-                    bestTargetPrevousIndex = unitPreviousIndex;
+                    Vector3 refererDirection = Vector3.ProjectOnPlane(referer.transform.forward, Vector3.up);
+                    Vector3 targetDirection = Vector3.ProjectOnPlane(unit.Position - referer.Position, Vector3.up);
+                    if (Vector3.Angle(refererDirection, targetDirection) > options.MaxReferringAngle)
+                        return;
+                }
+
+                SelectBestTarget(unit);
+            }
+
+            private void SelectBestTarget(Unit nextTarget)
+            {
+                switch (options.Distance)
+                {
+                    case TargetingDistance.Any:
+                        int unitPreviousIndex = previousTargets.IndexOf(nextTarget);
+                        if (BestTarget == null || unitPreviousIndex < bestTargetPrevousIndex)
+                        {
+                            BestTarget = nextTarget;
+                            bestTargetPrevousIndex = unitPreviousIndex;
+                        }
+                        break;
+                    case TargetingDistance.Near:
+                        float nextTargetNearDistance = referer.DistanceTo(nextTarget);
+                        if (BestTarget == null || nextTargetNearDistance < bestTargetDistance)
+                        {
+                            BestTarget = nextTarget;
+                            bestTargetDistance = nextTargetNearDistance;
+                        }
+                        break;
+                    case TargetingDistance.Far:
+                        float nextTargetFarDistance = referer.DistanceTo(nextTarget);
+                        if (BestTarget == null || nextTargetFarDistance > bestTargetDistance)
+                        {
+                            BestTarget = nextTarget;
+                            bestTargetDistance = nextTargetFarDistance;
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(options.Distance));
                 }
             }
         }
--- Assets/Scripts/Client/Input/Targeting/TargetingOptions.cs
+++ Assets/Scripts/Client/Input/Targeting/TargetingOptions.cs
@@ -12,10 +12,12 @@ namespace Client
         [SerializeField, UsedImplicitly, EnumFlag] private TargetingEntityType entityTypes;
         [SerializeField, UsedImplicitly] private TargetingDistance distance;
         [SerializeField, UsedImplicitly] private TargetingMode mode;
+        [SerializeField, UsedImplicitly] private float maxReferringAngle = 180.0f;
 
         public TargetingDeathState DeathState => deathState;
         public TargetingEntityType EntityTypes => entityTypes;
         public TargetingDistance Distance => distance;
         public TargetingMode Mode => mode;
+        public float MaxReferringAngle => maxReferringAngle;
     }
 }
