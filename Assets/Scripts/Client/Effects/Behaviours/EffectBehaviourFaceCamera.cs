732bebe5bd8eaaba570020a946cdc9ac850e155a
Reinisch <kvv.bsuir@gmail.com>
2019-08-18 00:07:07 +0300

    Added face camera effect behaviour

---
+++ Assets/Scripts/Client/Effects/Behaviours/EffectBehaviourFaceCamera.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Client
+{
+    public class EffectBehaviourFaceCamera : EffectBehaviour
+    {
+        [SerializeField, UsedImplicitly] private CameraReference cameraReference;
+        [SerializeField, UsedImplicitly] private Transform transformToRotate;
+        [SerializeField, UsedImplicitly] private Vector3 rotationOffset;
+
+        protected override void OnUpdate(IEffectEntity effectEntity, ref bool keepAlive)
+        {
+            base.OnUpdate(effectEntity, ref keepAlive);
+
+            if (cameraReference.WarcraftCamera != null)
+            {
+                Quaternion lookDirectionOffset = Quaternion.Euler(rotationOffset);
+                Vector3 projectedCameraDirection = Vector3.ProjectOnPlane(cameraReference.WarcraftCamera.transform.forward, Vector3.up);
+                transformToRotate.rotation = Quaternion.LookRotation(projectedCameraDirection) * lookDirectionOffset;
+            }
+        }
+    }
+}
