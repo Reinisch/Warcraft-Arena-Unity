61b13af7801df410954e503745252a94af416a9d
Reinisch <kvv.bsuir@gmail.com>
2019-08-19 20:48:18 +0300

    Added counterspell placeholder

--- Assets/Scripts/Game/Multiplayer/MultiplayerController.cs
+++ Assets/Scripts/Game/Multiplayer/MultiplayerController.cs
@@ -36,7 +36,7 @@ namespace Game
         private BoltConfig config;
         private State state;
 
-        public override string Version => "1.0.16";
+        public override string Version => "1.0.17";
 
         protected override void OnRegistered()
         {
