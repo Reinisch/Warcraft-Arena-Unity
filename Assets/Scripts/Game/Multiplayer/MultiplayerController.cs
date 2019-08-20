6213df71b085770fe2dbb43412f00ee028590b1f
Reinisch <kvv.bsuir@gmail.com>
2019-08-20 20:28:03 +0300

    Added polymorph spell placeholder

--- Assets/Scripts/Game/Multiplayer/MultiplayerController.cs
+++ Assets/Scripts/Game/Multiplayer/MultiplayerController.cs
@@ -36,7 +36,7 @@ namespace Game
         private BoltConfig config;
         private State state;
 
-        public override string Version => "1.0.17";
+        public override string Version => "1.0.18";
 
         protected override void OnRegistered()
         {
