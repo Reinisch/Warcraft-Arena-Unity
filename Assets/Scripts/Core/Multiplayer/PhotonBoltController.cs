79a7902948596d759d9f11b42cee813dad84e45a
Reinisch <kvv.bsuir@gmail.com>
2019-09-26 20:55:05 +0300

    Updated to Unity 2019.2.6f1

--- Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
+++ Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
@@ -34,7 +34,7 @@ namespace Core
         private State state;
 
         public Map<Guid, UdpSession> Sessions => BoltNetwork.SessionList;
-        public string Version => "1.0.39";
+        public string Version => "1.0.40";
 
         internal void Register()
         {
