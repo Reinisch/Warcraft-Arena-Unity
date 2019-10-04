162b0830db4d4c45143fbda3af25c7511000fbd1
Reinisch <kvv.bsuir@gmail.com>
2019-10-05 01:02:52 +0300

    AI players will now randomly spam emotes

---
+++ Assets/Scripts/Core/AI/Core/State Machine/Behaviours/DoRandomEmoteBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Common;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Core
+{
+    [UsedImplicitly]
+    public class DoRandomEmoteBehaviour : UnitStateMachineBehaviour
+    {
+        [SerializeField, UsedImplicitly] private List<EmoteType> randomEmotes;
+        [SerializeField, UsedImplicitly] private int emoteIntervalMin;
+        [SerializeField, UsedImplicitly] private int emoteIntervalMax;
+
+        private TimeTracker emoteTimeTracker;
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            emoteTimeTracker.Reset(RandomUtils.Next(emoteIntervalMin, emoteIntervalMax));
+        }
+
+        protected override void OnActiveUpdate(int deltaTime)
+        {
+            base.OnActiveUpdate(deltaTime);
+
+            emoteTimeTracker.Update(deltaTime);
+            if (emoteTimeTracker.Passed && randomEmotes.Count > 0)
+            {
+                Unit.ModifyEmoteState(RandomUtils.GetRandomElement(randomEmotes));
+                emoteTimeTracker.Reset(RandomUtils.Next(emoteIntervalMin, emoteIntervalMax));
+            }
+        }
+    }
+}
