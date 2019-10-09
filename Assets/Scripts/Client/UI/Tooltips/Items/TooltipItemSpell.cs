98d59828aac963d854632f4bdb6eac82fd1dba08
Reinisch <kvv.bsuir@gmail.com>
2019-10-09 20:39:47 +0300

    Added spell range, cooldown and cast time to tooltips

--- Assets/Scripts/Client/UI/Tooltips/Items/TooltipItemSpell.cs
+++ Assets/Scripts/Client/UI/Tooltips/Items/TooltipItemSpell.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using Client.Localization;
 using Core;
 using JetBrains.Annotations;
 using TMPro;
@@ -13,6 +14,13 @@ namespace Client
         [SerializeField, UsedImplicitly] private LocalizationReference localization;
         [SerializeField, UsedImplicitly] private TextMeshProUGUI spellDescription;
         [SerializeField, UsedImplicitly] private TextMeshProUGUI spellName;
+        [SerializeField, UsedImplicitly] private TextMeshProUGUI spellRange;
+        [SerializeField, UsedImplicitly] private TextMeshProUGUI spellCastTime;
+        [SerializeField, UsedImplicitly] private TextMeshProUGUI spellCooldown;
+        [SerializeField, UsedImplicitly] private LocalizedString rangeFormatString;
+        [SerializeField, UsedImplicitly] private LocalizedString cooldownFormatString;
+        [SerializeField, UsedImplicitly] private LocalizedString castTimeFormatString;
+        [SerializeField, UsedImplicitly] private LocalizedString castTimeInstantString;
         [SerializeField, UsedImplicitly] private Image spellIcon;
 
         private readonly object[] descriptionArguments = new object[SpellTooltipInfo.MaxArguments];
@@ -30,10 +38,26 @@ namespace Client
             {
                 spellName.text = tooltipInfo.SpellNameString.Value;
 
+                // update spell description with configured arguments
                 for (int i = 0; i < tooltipInfo.ArgumentSettings.Count; i++)
                     descriptionArguments[i] = tooltipInfo.ArgumentSettings[i].Resolve() ?? unknownArguments[i];
 
+                for (int i = tooltipInfo.ArgumentSettings.Count; i < unknownArguments.Length; i++)
+                    descriptionArguments[i] = unknownArguments[i];
+
                 spellDescription.text = string.Format(tooltipNumberFormat, tooltipInfo.SpellDescriptionString.Value, descriptionArguments);
+
+                // update spell range label
+                float range = Mathf.Max(spellInfo.MaxRangeFriend, spellInfo.MaxRangeHostile);
+                spellRange.text = Mathf.Approximately(range, 0.0f) ? string.Empty : string.Format(rangeFormatString.Value, range);
+
+                // update cooldown label
+                float cooldown = (float) spellInfo.CooldownTime / 1000;
+                spellCooldown.text = cooldown <= 0 ? string.Empty : string.Format(cooldownFormatString.Value, cooldown);
+
+                // update cast time label
+                float castTime = (float)spellInfo.CastTime / 1000;
+                spellCastTime.text = castTime <= 0 ? castTimeInstantString.Value : string.Format(castTimeFormatString.Value, castTime);
 
                 return true;
             }
