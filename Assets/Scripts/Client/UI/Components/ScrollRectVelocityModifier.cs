71166da3dea26d203703b76d125308798c93a23a
Reinisch <kvv.bsuir@gmail.com>
2019-10-15 23:22:29 +0300

    Added handlers for chat scroll buttons

---
+++ Assets/Scripts/Client/UI/Components/ScrollRectVelocityModifier.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+[UsedImplicitly]
+public class ScrollRectVelocityModifier : MonoBehaviour
+{
+    [SerializeField, UsedImplicitly] private ScrollRect targetScrollRect;
+    [SerializeField, UsedImplicitly] private Vector2 velocity;
+    [SerializeField, UsedImplicitly] private bool repeatWithDelay;
+    [SerializeField, UsedImplicitly] private float repeatVelocityDelay = 0.1f;
+
+    [UsedImplicitly]
+    public void Forward()
+    {
+        ModifyVelocityForward();
+
+        if (repeatWithDelay)
+        {
+            CancelInvoke();
+            Invoke(nameof(ModifyVelocityForward), repeatVelocityDelay);
+        }
+    }
+
+    [UsedImplicitly]
+    public void Backward()
+    {
+        ModifyVelocityBackward();
+
+        if (repeatWithDelay)
+        {
+            CancelInvoke();
+            Invoke(nameof(ModifyVelocityBackward), repeatVelocityDelay);
+        }
+    }
+
+    private void ModifyVelocityForward() => targetScrollRect.velocity = velocity;
+
+    private void ModifyVelocityBackward() => targetScrollRect.velocity = -velocity;
+}
