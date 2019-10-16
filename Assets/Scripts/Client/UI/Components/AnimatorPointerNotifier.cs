2839bb0bcc6531e3c3f8af15a8dad7a28bf03de9
Reinisch <kvv.bsuir@gmail.com>
2019-10-16 19:52:13 +0300

    Added chat alpha transition on mouseover

---
+++ Assets/Scripts/Client/UI/Components/AnimatorPointerNotifier.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[UsedImplicitly]
+public class AnimatorPointerNotifier : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, UsedImplicitly] private Animator animator;
+    [SerializeField, UsedImplicitly] private string parameter;
+
+    [UsedImplicitly]
+    private void OnDisable() => animator.SetBool(parameter, false);
+
+    public void OnPointerEnter(PointerEventData eventData) => animator.SetBool(parameter, true);
+
+    public void OnPointerExit(PointerEventData eventData) => animator.SetBool(parameter, false);
+}
