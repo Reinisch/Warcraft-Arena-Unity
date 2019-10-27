543f05f08b4902a5ce20a9b9c4ab8fac58b09da7
Reinisch <kvv.bsuir@gmail.com>
2019-10-27 13:56:42 +0300

    Reworked unit animator, deleted strafing animations

---
+++ Assets/Scripts/Client/Animation/State Machine Behaviours/AnimatorSetFloatBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Client
+{
+    public class AnimatorSetFloatBehaviour : StateMachineBehaviour
+    {
+        [SerializeField, UsedImplicitly] private string parameterName;
+        [SerializeField, UsedImplicitly] private float valueOnEnter;
+        [SerializeField, UsedImplicitly] private float valueOnExit;
+        [SerializeField, UsedImplicitly] private bool setOnEnter = true;
+        [SerializeField, UsedImplicitly] private bool setOnExit = true;
+
+        private int parameterHash;
+
+        [UsedImplicitly]
+        private void OnEnable()
+        {
+            parameterHash = Animator.StringToHash(parameterName);
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+        {
+            if (setOnEnter)
+                animator.SetFloat(parameterHash, valueOnEnter);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+        {
+            if (setOnExit)
+                animator.SetFloat(parameterHash, valueOnExit);
+        }
+    }
+}
