78abbbdec30869c211ee70372ba689e49097a039
Reinisch <kvv.bsuir@gmail.com>
2019-10-27 15:00:18 +0300

    Synchronized body and legs when instantly casting while running

---
+++ Assets/Scripts/Client/Animation/State Machine Behaviours/AnimatorSyncNormalizedTimeBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Client
+{
+    public class AnimatorSyncNormalizedTimeBehaviour : StateMachineBehaviour
+    {
+        [SerializeField, UsedImplicitly] private int otherLayerIndex;
+
+        private bool alreadySynced;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+        {
+            if (alreadySynced)
+                alreadySynced = false;
+            else
+            {
+                AnimatorStateInfo otherStateInfo = animator.GetCurrentAnimatorStateInfo(otherLayerIndex);
+
+                if (otherStateInfo.shortNameHash == stateInfo.shortNameHash)
+                {
+                    alreadySynced = true;
+                    animator.CrossFade(stateInfo.shortNameHash, 0.2f, layerIndex, otherStateInfo.normalizedTime);
+                }
+            }
+        }
+    }
+}
