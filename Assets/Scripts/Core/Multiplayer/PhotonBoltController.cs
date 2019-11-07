40ed90599f03169a8a0b93962d8c914409700a9f
Reinisch <kvv.bsuir@gmail.com>
2019-11-07 16:28:27 +0300

    Added Survival Instincts for druid

--- Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
+++ Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
@@ -34,7 +34,7 @@ namespace Core
         private State state;
 
         public Map<Guid, UdpSession> Sessions => BoltNetwork.SessionList;
-        public string Version => "1.0.57";
+        public string Version => "1.0.58";
 
         internal void Register()
         {
