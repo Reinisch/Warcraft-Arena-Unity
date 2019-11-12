6515c59f1335166f3f20979b71b8c71eb1122fdb
Reinisch <kvv.bsuir@gmail.com>
2019-11-12 21:51:09 +0300

    Added Savage Roar spell for druid

--- Assets/Scripts/Core/Spells/Spell Auras/Aura Effects/AuraEffectInfoModifyPowerRegenPercent.cs
+++ Assets/Scripts/Core/Spells/Spell Auras/Aura Effects/AuraEffectInfoModifyPowerRegenPercent.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Core.AuraEffects
 {
-    [UsedImplicitly, CreateAssetMenu(fileName = "Aura Effect Modify Power Regen Percent", menuName = "Game Data/Spells/Auras/Effects/Modify Power Regen percent", order = 3)]
+    [UsedImplicitly, CreateAssetMenu(fileName = "Aura Effect Modify Power Regen Percent", menuName = "Game Data/Spells/Auras/Effects/Modify Power Regen Percent", order = 3)]
     public class AuraEffectInfoModifyPowerRegenPercent : AuraEffectInfo
     {
         [SerializeField, UsedImplicitly, Range(-100f, 500f)] private float powerRegenPercent;
