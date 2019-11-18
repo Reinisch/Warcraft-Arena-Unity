8594023f74d51db64ac76a34cb26e855d38a3e39
lbalotta <lbalotta@transferoil.com>
2019-11-18 12:16:25 +0100

    [DB/Accounts] Base accounts

---
+++ Database/InitData.sql
@@ -0,0 +1,5 @@
+-- Insert 2 default accounts:
+-- 1 -> Username: admin, Password: admin
+-- 2 -> Username: test, Password: test
+INSERT INTO accounts(Username, Password) VALUES ('admin', '8301316D0D8448A34FA6D0C6BF1CBFA2B4A1A93A');
+INSERT INTO accounts(Username, Password) VALUES ('test', '3D0D99423E31FCC67A6745EC89D70D700344BC76');
