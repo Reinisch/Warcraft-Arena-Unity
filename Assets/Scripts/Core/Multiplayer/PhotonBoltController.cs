c5714ad6b68d5ab7bd372e684277dd8926eb4306
Reinisch <kvv.bsuir@gmail.com>
2019-12-11 22:07:42 +0300

    Added slow fall spell for mage

--- Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
+++ Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
@@ -34,7 +34,7 @@ namespace Core
         private State state;
 
         public Map<Guid, UdpSession> Sessions => BoltNetwork.SessionList;
-        public string Version => "1.0.79";
+        public string Version => "1.0.80";
 
         internal void Register()
         {
