1db120dd943f68a94b55abf93dcb87f132051d0c
Reinisch <kvv.bsuir@gmail.com>
2020-01-09 23:37:45 +0300

    Added cyclone spell for druid

--- Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
+++ Assets/Scripts/Core/Multiplayer/PhotonBoltController.cs
@@ -34,7 +34,7 @@ namespace Core
         private State state;
 
         public Map<Guid, UdpSession> Sessions => BoltNetwork.SessionList;
-        public string Version => "1.0.87";
+        public string Version => "1.0.88";
 
         internal void Register()
         {
