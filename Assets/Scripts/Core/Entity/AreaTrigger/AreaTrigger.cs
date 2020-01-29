3797bc348142d93e71d59795991ff11cfd911a22
Reinisch <kvv.bsuir@gmail.com>
2020-01-29 22:12:06 +0300

    Deleted unused entities

--- Assets/Scripts/Core/Entity/AreaTrigger/AreaTrigger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core
-{
-    public class AreaTrigger : WorldEntity
-    {
-        internal override bool AutoScoped => true;
-
-        public override string Name { get; internal set; }
-    }
-}
--- Assets/Scripts/Core/Entity/Corpse/Corpse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core
-{
-    public class Corpse : WorldEntity
-    {
-        internal override bool AutoScoped => true;
-
-        public override string Name { get; internal set; }
-    }
-}
